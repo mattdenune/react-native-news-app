588bece1752eb4d59bc2c8f4266b847c7d10d7f3
Matthew Denune <mattdenune@gmail.com>
2021-11-03 16:20:44 -0400

    finished Card.js layout

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import  AppLoading  from "expo-app-loading";
 
 import Header from './src/components/Header';
 import Card from './src/components/Card';
 
+const loadFonts = () => {
+  return Font.loadAsync({
+    'Ubuntu': require('./assets/fonts/Ubuntu-Regular.ttf'),
+    'Ubuntu-Bold': require('./assets/fonts/Ubuntu-Bold.ttf'),
+  })
+}
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false)
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={loadFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  };
+
   return (
     <View >
       <Header />
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,44 +1,71 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Card = () => {
   return (
     <View style={styles.card}>
       <View style={styles.imageWrapper}>
-        
+        <Image
+          source={require("../../assets/news-dice.jpeg")}
+          style={styles.image}
+        />
       </View>
       <View style={styles.titleWrapper}>
-        <Text>Dummy Title</Text>
+        <Text style={styles.title}>Dummy Title</Text>
+        <MaterialIcons name="favorite-border" size={24} color="#72bcd4" />
       </View>
       <View style={styles.descriptionWrapper}>
-        <Text>Dummy Description</Text>
+        <Text style={styles.description}>Dummy Description</Text>
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: '#ffffff',
+    backgroundColor: "#ffffff",
     height: 300,
     margin: 20,
     borderRadius: 10,
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowOpacity: 0.25,
-    shadowOffset: {width: 0, height: 2},
+    shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
     elevation: 5,
   },
   imageWrapper: {
-    width: '100%',
-    height: '60%',
+    width: "100%",
+    height: "60%",
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
-  titleWrapper: {},
-  descriptionWrapper: {},
-})
+  titleWrapper: {
+    height: "10%",
+    paddingHorizontal: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  descriptionWrapper: {
+    paddingHorizontal: 15,
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+  },
+  title: {
+    fontFamily: "Ubuntu-Bold",
+    fontSize: 20,
+    marginTop: 10,
+  },
+  description: {
+    fontFamily: "Ubuntu",
+    fontSize: 15,
+    marginTop: 10,
+  },
+});
 
 export default Card
 
