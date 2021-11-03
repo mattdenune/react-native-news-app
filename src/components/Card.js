78b2e834d5b4ce030f883109cd0a763164cffa8a
Matthew Denune <mattdenune@gmail.com>
2021-11-03 13:40:38 -0400

    building out card and header components

---
+++ src/components/Card.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Card = () => {
+  return (
+    <View style={styles.card}>
+      <View style={styles.imageWrapper}>
+        
+      </View>
+      <View style={styles.titleWrapper}>
+        <Text>Dummy Title</Text>
+      </View>
+      <View style={styles.descriptionWrapper}>
+        <Text>Dummy Description</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#ffffff',
+    height: 300,
+    margin: 20,
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOpacity: 0.25,
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 8,
+    elevation: 5,
+  },
+  imageWrapper: {
+    width: '100%',
+    height: '60%',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    overflow: 'hidden',
+  },
+  titleWrapper: {},
+  descriptionWrapper: {},
+})
+
+export default Card
+
