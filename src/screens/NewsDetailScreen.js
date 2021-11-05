801bf3cf7a76440461de0892378c725d440fe6e1
Matthew Denune <mattdenune@gmail.com>
2021-11-05 17:01:01 -0400

    finished news App

--- src/screens/NewsDetailScreen.js
+++ src/screens/NewsDetailScreen.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import * as newsAction from "../redux/actions/newsAction";
 
 const NewsDetailScreen = ({ route }) => {
 
+  const dispatch = useDispatch();
   const {articleUrl} = route.params;
 
   const article = useSelector(state => state.news.articles.articles.find(article => article.url === articleUrl))
   
+  const isFav = useSelector((state) =>
+    state.news.favorites.some((article) => article.url === article.url)
+  );
 
   return (
     <View style={styles.container}>
@@ -16,10 +22,20 @@ const NewsDetailScreen = ({ route }) => {
         <Text style={styles.title}>{article.title}</Text>
       </View>
       <View>
-        <ImageBackground style={styles.image} source={{uri: article.urlToImage}}>
+        <ImageBackground
+          style={styles.image}
+          source={{ uri: article.urlToImage }}
+        >
           <View style={styles.titleContainer}>
             <Text style={styles.author}>{article.author}</Text>
-            <MaterialIcons/>
+            <MaterialIcons
+              name={isFav ? "favorite" : "favorite-border"}
+              size={24}
+              color="#72bcd4"
+              onPress={() => {
+                dispatch(newsAction.toggleFavorites(article.url));
+              }}
+            />
           </View>
         </ImageBackground>
       </View>
@@ -27,7 +43,7 @@ const NewsDetailScreen = ({ route }) => {
         <Text style={styles.descriptionText}>{article.description}</Text>
       </View>
     </View>
-  )
+  );
 }
 
 export default NewsDetailScreen
